9e43d8ad780fb0c661aa8a0c6ac78e9bba9d5ddf
thanhtai18021994 <taibui18021994@gmail.com>
2021-08-30 08:48:56 +0700

    update

--- shopComputer/BE/data/feature-product.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM shoplaptop.feature_product_product;
-delimiter //
-create procedure insertFeatureProductProduct(in param1 int,in param2 int)
-begin
-insert into feature_product_product
-values(param1,param2);
-end //
-delimiter ;
-call insertFeatureProductProduct(1,10)
